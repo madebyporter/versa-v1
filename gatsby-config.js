fc2c961e9a018ed5dec985f23b0f04f5537b5d0a
madebyporter <general@madebyporter.com>
2021-11-19 13:52:46 -0500

    updated description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Made by Porter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Minimalist designer, developer, music producer &amp; entrepreneur.`,
     author: `@madebyporter`,
     siteUrl: `https://madebyporter.com`,
   },
