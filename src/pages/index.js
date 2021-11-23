782fd94ee5bdad7f8cfa442c0290105410ca9bc4
madebyporter <general@madebyporter.com>
2021-11-22 21:06:47 -0500

    added ssfn link

--- src/pages/index.js
+++ src/pages/index.js
@@ -84,7 +84,7 @@ const IndexPage = () => (
           <li>
             <ul className="list-item-columns g-align-items-end">
               <li className="t-24 g-m-1-13 g-t-1-5 g-l-1-4 g-1-4">Faithful Homes</li>
-              <li className="t-18 g-m-1-8 g-t-5-10 g-l-5-9 g-5-9">Product Design <small>w/ SSFN</small></li>
+              <li className="t-18 g-m-1-8 g-t-5-10 g-l-5-9 g-5-9">Product Design <small>w/ <a href="https://www.ssfn.io" target="_blank">SSFN</a></small></li>
               <li className="t-18 g-m-8-12 g-t-10-12 g-l-10-12 g-10-12">2018–</li>
               <li className="t-18 g-m-12-13 g-t-12-13 g-l-12-13 g-12-13 g-align-items-center"><span className="status status-active-full" /></li>
             </ul>
