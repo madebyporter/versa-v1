9d574acce5b6252745e4c93aff886d57500ace75
madebyporter <general@madebyporter.com>
2021-11-28 20:58:39 -0500

    added jose espinoza link

--- src/pages/index.js
+++ src/pages/index.js
@@ -211,7 +211,7 @@ const IndexPage = () => (
           <li>
             <ul className="list-item-columns g-align-items-end">
               <li className="t-24 g-m-1-13 g-t-1-5 g-l-1-5 g-1-4">I Fly You Buy</li>
-              <li className="t-18 g-m-1-8 g-t-5-10 g-l-5-9 g-5-9">Product Design <small>w/ Jose Espinosa</small></li>
+              <li className="t-18 g-m-1-8 g-t-5-10 g-l-5-9 g-5-9">Product Design <small>w/ <a href="https://itsmejose.com/" target="_blank" rel="noreferrer">Jose Espinosa</a></small></li>
               <li className="t-18 g-m-8-12 g-t-10-12 g-l-10-12 g-10-12">2017–2018</li>
               <li className="t-18 g-m-12-13 g-t-12-13 g-l-12-13 g-12-13 g-align-items-center"><span className="status status-dead" /></li>
             </ul>
@@ -229,7 +229,7 @@ const IndexPage = () => (
               <li className="t-24 g-m-1-13 g-t-1-5 g-l-1-5 g-1-5"><a href="https://www.myfdtps.com" target="_blank" rel="noreferrer">Foundation Transportation</a></li>
               <li className="t-18 g-m-1-8 g-t-5-10 g-l-5-9 g-5-9">Web Design &amp; Consultant</li>
               <li className="t-18 g-m-8-12 g-t-10-12 g-l-10-12 g-10-12">2016-2019</li>
-              <li className="t-18 g-m-12-13 g-t-12-13 g-l-12-13 g-12-13 g-align-items-center"><span className="status status-dead" /></li>
+              <li className="t-18 g-m-12-13 g-t-12-13 g-l-12-13 g-12-13 g-align-items-center"><span className="status status-active-partial" /></li>
             </ul>
           </li>
           <li>
