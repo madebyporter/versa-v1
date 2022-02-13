80acd1a00e36a3e14d49b7377683e770b3360bd7
madebyporter <general@madebyporter.com>
2022-02-13 15:35:15 -0800

    add initial case study template

--- src/pages/about.js
+++ src/pages/about.js
@@ -65,7 +65,7 @@ const IndexPage = () => (
           </li>
           <li>
             <ul className="list-item-columns g-align-items-end">
-              <li className="t-24 g-m-1-13 g-t-1-5 g-l-1-5 g-1-4"><a href="https://www.organicsalivegarden.com" target="_blank" rel="noreferrer">Organics Alive</a></li>
+              <li className="t-24 g-m-1-13 g-t-1-5 g-l-1-5 g-1-4"><a href="/cases/organicsalive-2019" rel="noreferrer">Organics Alive</a></li>
               <li className="t-18 g-m-1-8 g-t-5-10 g-l-5-9 g-5-9">Shopify Consultant <small>w/ <a href="https://www.ssfn.io" target="_blank" rel="noreferrer">SSFN</a></small></li>
               <li className="t-18 g-m-8-12 g-t-10-12 g-l-10-12 g-10-12">2018–</li>
               <li className="t-18 g-m-12-13 g-t-12-13 g-l-12-13 g-12-13 g-align-items-center"><span className="status status-active-full" /></li>
---
+++ src/pages/cases/organicsalive-2019.js
@@ -0,0 +1,141 @@
+import * as React from "react"
+import { Link } from "gatsby"
+// import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../../components/layout"
+import Seo from "../../components/seo"
+import Hero from "../../components/hero"
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleArrowUp } from '@fortawesome/pro-solid-svg-icons'
+import { faCircleArrowDown } from '@fortawesome/pro-solid-svg-icons'
+
+import oahomepage from "../../images/cases/organicsalive-2019-homepage.png"
+import oahomepagemobile from "../../images/cases/organicsalive-2019-homepage-mobile.png"
+import oaproductdetailmobile from "../../images/cases/organicsalive-2019-productdetail-mobile.png"
+
+const IndexPage = () => (
+  
+  <Layout>
+    <Seo title="Case Study: Organics Alive" />
+
+    <Hero heroText="Organics Alive" heroDesc="A case study on how we moved Organics Alive from WooCommerce to Shopify to improve processes."  />
+
+    <section className="block block-section padding-top-none">
+      <div className="block-portfolio block-portfolio-desktop g-m-1-13">
+        <img className="block-portfolio-img" src={oahomepage} alt="Portfolio" />  
+      </div>
+      <div className="block-content g-m-1-13 g-l-4-12 g-4-12">
+        <h3 className="t-36 block-content-title">Shopify Development &amp; Consulting</h3>
+        <p className="t-36">Our goal here was to move Organics Alive from WooCommerce to Shopify. <a href="https://www.creativepollinator.com/" target="_blank" rel="noreferrer">Jennie McGuirk</a> created the design direction &amp; branding. I helped setup the Shopify system, pages &amp; processes.</p>
+        <a className="t-36" href="https://www.organicsalivegarden.com" target="_blank" rel="noreferrer">View Website</a>
+      </div>
+
+      <div className="block-content g-m-1-13 g-l-4-12 g-4-12">
+        <header className="block-content-header grid">
+          <h3 className="t-36 block-content-title g-m-1-13 g-t-1-7">Process</h3>
+        </header>
+        
+        <ul className="list-bordered resume-list">
+          <li>
+            <ul className="list-item-columns g-align-items-start">
+              <li className="t-24 g-m-1-13 g-t-1-5 g-l-1-4 g-1-4">Discovery</li>
+              <li className="t-18 g-m-1-13 g-t-5-13 g-l-5-13 g-5-13">Review old site, understand problem, review new design material &amp; connecting the dots.</li>
+            </ul>
+          </li>
+          <li>
+            <ul className="list-item-columns g-align-items-start">
+              <li className="t-24 g-m-1-13 g-t-1-5 g-l-1-4 g-1-4">Setup the System</li>
+              <li className="t-18 g-m-1-13 g-t-5-13 g-l-5-13 g-5-13">Created a new Shopify account, configured store details, users, payment, checkout, shipping, delivery &amp; notifications.</li>
+            </ul>
+          </li>
+          <li>
+            <ul className="list-item-columns g-align-items-start">
+              <li className="t-24 g-m-1-13 g-t-1-5 g-l-1-4 g-1-4">Setup store layout</li>
+              <li className="t-18 g-m-1-13 g-t-5-13 g-l-5-13 g-5-13">Installed theme, adjusted CSS styles to match layout &amp; adjusted Liquid templates to tinker with store logic.</li>
+            </ul>
+          </li>
+          <li>
+            <ul className="list-item-columns g-align-items-start">
+              <li className="t-24 g-m-1-13 g-t-1-5 g-l-1-4 g-1-4">Setup functionality &amp; automation</li>
+              <li className="t-18 g-m-1-13 g-t-5-13 g-l-5-13 g-5-13">Integrated Mailchimp for email marketing automation; Integrated GoAffPro for affiliate marketing; Integrated Metafields Guru for product page customization; Integrated Store Locator to map distributors.</li>
+            </ul>
+          </li>
+          <li>
+            <ul className="list-item-columns g-align-items-start">
+              <li className="t-24 g-m-1-13 g-t-1-5 g-l-1-4 g-1-4">Test &amp; Ship</li>
+              <li className="t-18 g-m-1-13 g-t-5-13 g-l-5-13 g-5-13">Tested the site on modern browsers &amp; devices; Integrated Google Analytics &amp; Shipped within two months.</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+
+      <div className="block-portfolio block-portfolio-mobile-two g-m-1-13">
+        <img className="block-portfolio-img" src={oahomepagemobile} alt="Portfolio" />  
+        <img className="block-portfolio-img" src={oaproductdetailmobile} alt="Portfolio" />  
+      </div>
+
+      <div className="block-content g-m-1-13 g-l-4-12 g-4-12">
+        <header className="block-content-header grid">
+          <h3 className="t-36 block-content-title g-m-1-13 g-t-1-7">Results</h3>
+        </header>
+        
+        <ul className="list-bordered resume-list">
+          <li>
+            <ul className="list-item-columns g-align-items-end">
+              <li className="t-24 g-m-1-13 g-t-1-5 g-l-1-4 g-1-4">Sales</li>
+              <li className="t-18 g-m-1-7 g-t-1-7 g-l-1-7 g-5-10">
+                $250,000 <small>(2021)</small>
+                <span className="data-change dc-up"><FontAwesomeIcon icon={faCircleArrowUp} /> 25% y/y</span>
+              </li>
+              <li className="t-18 g-m-7-13 g-t-7-13 g-l-7-13 g-10-13">$200,000 <small>(2020)</small></li>
+            </ul>
+          </li>
+          <li>
+            <ul className="list-item-columns g-align-items-end">
+              <li className="t-24 g-m-1-13 g-t-1-5 g-l-1-4 g-1-4">Sessions</li>
+              <li className="t-18 g-m-1-7 g-t-1-7 g-l-1-7 g-5-10">
+                36,713 <small>(2021)</small>
+                <span className="data-change dc-down"><FontAwesomeIcon icon={faCircleArrowDown} /> 10% y/y</span>
+              </li>
+              <li className="t-18 g-m-7-13 g-t-7-13 g-l-7-13 g-10-13">40,829 <small>(2020)</small></li>
+            </ul>
+          </li>
+          <li>
+            <ul className="list-item-columns g-align-items-end">
+              <li className="t-24 g-m-1-13 g-t-1-5 g-l-1-4 g-1-4">Conversion Rate</li>
+              <li className="t-18 g-m-1-7 g-t-1-7 g-l-1-7 g-5-10">
+                2.53% <small>(2021)</small>
+                <span className="data-change dc-up"><FontAwesomeIcon icon={faCircleArrowUp} /> 24% y/y</span>
+              </li>
+              <li className="t-18 g-m-7-13 g-t-7-13 g-l-7-13 g-10-13">2.05% <small>(2020)</small></li>
+            </ul>
+          </li>
+          <li>
+            <ul className="list-item-columns g-align-items-end">
+              <li className="t-24 g-m-1-13 g-t-1-5 g-l-1-4 g-1-4">Returning Customers</li>
+              <li className="t-18 g-m-1-7 g-t-1-7 g-l-1-7 g-5-10">
+                34.51% <small>(2021)</small>
+                <span className="data-change dc-up"><FontAwesomeIcon icon={faCircleArrowUp} /> 58% y/y</span>
+              </li>
+              <li className="t-18 g-m-7-13 g-t-7-13 g-l-7-13 g-10-13">21.9% <small>(2020)</small></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div className="block-content g-m-1-13 g-l-4-12 g-4-12">
+        <h3 className="t-36 block-content-title">Testimonial</h3>
+        <blockquote className="blockquote t-24">
+          "It runs smoothly. We do about 250k with zero advertising!"
+          <p className="t-18"><em>- Todd Salemi, Founder &amp; President of Organics Alive</em></p>
+        </blockquote>
+        
+        <a className="t-18" href="https://www.organicsalivegarden.com" target="_blank" rel="noreferrer">View Website</a>
+      </div>
+    </section>
+
+    
+  </Layout>
+)
+
+export default IndexPage
