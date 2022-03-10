2b051f9b1f39acccf49686067a093cd9c1cc32ce
madebyporter <general@madebyporter.com>
2022-03-09 17:40:50 -0800

    added new menu icons

--- src/components/navGlobal.js
+++ src/components/navGlobal.js
@@ -5,13 +5,13 @@ import NavbarLinks from "./navLinks"
 import { motion } from "framer-motion"
 
 const Navbar = () => {
-  
+
   const [navbarOpen, setNavbarOpen] = useState(false)
 
   return (
     <div className="nav-component">
       <div className="nav-trigger" navbarOpen={navbarOpen} onClick={() => setNavbarOpen(!navbarOpen)}>
-        {navbarOpen ? <div className="nav-trigger-icon nav-trigger-open" open>&times;</div> : <div className="nav-trigger-icon nav-trigger-closed">bars</div>}
+        {navbarOpen ? <div className="nav-trigger-icon nav-trigger-open" open></div> : <div className="nav-trigger-icon nav-trigger-closed"></div>}
       </div>
 
       {navbarOpen ? (
