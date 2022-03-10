3212bf3d686588bec79f7a0c6b3beb9184a6ce65
madebyporter <general@madebyporter.com>
2022-03-09 19:29:14 -0800

    fixed myfdtps link

--- src/pages/about.js
+++ src/pages/about.js
@@ -193,7 +193,7 @@ const IndexPage = () => (
           </li>
           <li>
             <ul className="list-item-columns g-align-items-end">
-              <li className="t-24 g-m-1-13 g-t-1-5 g-l-1-5 g-1-5"><a href="https://www.myfdtps.com" target="_blank" rel="noreferrer">Foundation Transportation</a></li>
+              <li className="t-24 g-m-1-13 g-t-1-5 g-l-1-5 g-1-5"><a href="https://myfdtps.netlify.app/" target="_blank" rel="noreferrer">Foundation Transportation</a></li>
               <li className="t-18 g-m-1-8 g-t-5-10 g-l-5-9 g-5-9">Web Design &amp; Consultant</li>
               <li className="t-18 g-m-8-12 g-t-10-12 g-l-10-12 g-10-12">2016-2019</li>
               <li className="t-18 g-m-12-13 g-t-12-13 g-l-12-13 g-12-13 g-align-items-center"><span className="status status-yellow" /></li>
