d33184e3feac3dca34e215049d9acd2f999eeea2
madebyporter <general@madebyporter.com>
2023-01-22 10:33:34 -0800

    changed navigation from link to a

--- src/components/navLinks.js
+++ src/components/navLinks.js
@@ -32,21 +32,23 @@ const NavbarLinks = () => {
     <>
       <motion.div className="nav-section" variants={container} initial="hidden" animate="show">
 
-        <motion.h3 className="nav-section-header" variants={item}>Specialties</motion.h3>
+        <motion.h3 className="nav-section-header" variants={item}>Disciplines</motion.h3>
         <motion.div className="nav-link" variants={item}>
-          <Link to="/design/">Design</Link>
+          <a href="/design/">Design Engineering</a>
         </motion.div>
         <motion.div className="nav-link" variants={item}>
-          <Link to="/stack/">Stack</Link>
-        </motion.div>
-        <motion.div className="nav-link" variants={item}>
-          <Link to="/about/">About</Link>
+          <a className="nav-link nav-link-external" href="https://www.thebeatboxclub.com/producers/madebyporter" target="_blank">Music Production</a>
         </motion.div>
    
-        <motion.h3 className="nav-section-header" variants={item}>Products</motion.h3>
-        <motion.a className="nav-link nav-link-external" href="https://madebyporter.gumroad.com/l/projective" target="_blank" variants={item}>Projective</motion.a>
-        <motion.a className="nav-link nav-link-external" href="https://calendly.com/madebyporter/consulting" target="_blank" variants={item}>Mentorship</motion.a>
-        <motion.a className="nav-link nav-link-external" href="https://www.thebeatboxclub.com/producers/madebyporter" target="_blank" variants={item}>Beats</motion.a>
+        <motion.h3 className="nav-section-header" variants={item}>Person</motion.h3>
+        
+        <motion.div className="nav-link" variants={item}>
+          <a href="/stack/">Stack</a>
+        </motion.div>
+        <motion.div className="nav-link" variants={item}>
+          <a href="/about/">About</a>
+        </motion.div>
+
       </motion.div>
     </>
   )
